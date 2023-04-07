81104ac4a82fccfc48ccd50099550fa1ac1abb3f
Alexey <prostojonny@beatspro.ru>
2023-04-07 14:05:21 +0400

    add DZ_8

---
+++ Program.cs
@@ -0,0 +1,227 @@
+﻿//--------------54----------------
+
+// Console.WriteLine("введите количество строк");
+// int linesVol = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите количество столбцов");
+// int columnsVol = Convert.ToInt32(Console.ReadLine());
+// int[,] numbers = new int[linesVol, columnsVol];
+// FillArrayRandomNumbers(numbers);
+// Console.WriteLine();
+// Console.WriteLine("Массив до изменения");
+// PrintArray(numbers);
+
+// for (int i = 0; i < numbers.GetLength(0); i++)
+// {
+//     for (int j = 0; j < numbers.GetLength(1) - 1; j++)
+//     {
+//         for (int z = 0; z < numbers.GetLength(1) - 1; z++)
+//         {
+//             if (numbers[i, z] < numbers[i, z + 1]) //для изменения сортировки поменять знак < на противоположный
+//             {
+//                 int temp = 0;
+//                 temp = numbers[i, z];
+//                 numbers[i, z] = numbers[i, z + 1];
+//                 numbers[i, z + 1] = temp;
+//             }
+//         }
+//     }
+// }
+// Console.WriteLine();
+// Console.WriteLine("Массив с упорядоченными значениями");
+// PrintArray(numbers);
+
+// void FillArrayRandomNumbers(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = new Random().Next(0, 10);
+//         }
+//     }
+// }
+
+// void PrintArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         Console.Write("[ ");
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.Write("]");
+//         Console.WriteLine("");
+//     }
+// }
+
+
+// ----------------58-----------------
+
+// int InputInt(string output)
+// {
+//     Console.Write(output);
+//     return int.Parse(Console.ReadLine());
+// }
+
+// void FillArrayRandomNumbers(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = new Random().Next(1, 5); //Для увеличения размера чисел в матрицах поменять число 5 на большее
+//         }
+//     }
+// }
+
+// void PrintArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         Console.Write("[ ");
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.Write("]");
+//         Console.WriteLine("");
+//     }
+// }
+
+// int size = InputInt("размерность матриц: ");
+// int[,] matrixA = new int[size, size];
+// int[,] matrixB = new int[size, size];
+// FillArrayRandomNumbers(matrixA);
+// FillArrayRandomNumbers(matrixB);
+// int[,] matrixC = new int[size, size];
+
+// for (int i = 0; i < size; i++)
+// {
+//     for (int j = 0; j < size; j++)
+//     {
+//         for (int k = 0; k < size; k++)
+//         {
+//             matrixC[i, j] = matrixC[i, j] + (matrixA[i, k] * matrixB[k, j]);
+//         }
+//     }
+// }
+// Console.WriteLine("Матрица - А");
+// PrintArray(matrixA);
+// Console.WriteLine();
+// Console.WriteLine("Матрица - В");
+// PrintArray(matrixB);
+// Console.WriteLine();
+// Console.WriteLine("Произведение матриц А*В");
+// PrintArray(matrixC);
+
+// -----------60----------------
+
+// int deep1 = InputInt("Введите размерность 1: ");
+// int deep2 = InputInt("Введите размерность 2: ");
+// int deep3 = InputInt("Введите размерность 3: ");
+// int countNums = 89;
+
+// if (deep1 * deep2 * deep3 > countNums)
+// {
+//     Console.Write("Массив слишком большой");
+//     return;
+// }
+
+// int[,,] resultNums = Create3DMassive(deep1, deep2, deep3);
+
+// for (int i = 0; i < resultNums.GetLength(0); i++)
+// {
+//     for (int j = 0; j < resultNums.GetLength(1); j++)
+//     {
+//         for (int k = 0; k < resultNums.GetLength(2); k++)
+//         {
+//             Console.WriteLine($"[{i},{j},{k}] - {resultNums[i, j, k]}");
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+
+
+// int[,,] Create3DMassive(int size1, int size2, int size3)
+// {
+//     int[,,] array = new int[size1, size2, size3];
+//     int[] values = new int[countNums];
+//     int num
+//      = 10;
+//     for (int i = 0; i < values.Length; i++)
+//         values[i] = num
+//         ++;
+
+//     for (int i = 0; i < values.Length; i++)
+//     {
+//         int randomInd = new Random().Next(0, values.Length);
+//         int temp = values[i];
+//         values[i] = values[randomInd];
+//         values[randomInd] = temp;
+//     }
+
+//     int valueIndex = 0;
+
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             for (int k = 0; k < array.GetLength(2); k++)
+//             {
+//                 array[i, j, k] = values[valueIndex++];
+//             }
+//         }
+//     }
+//     return array;
+// }
+
+
+// int InputInt(string output)
+// {
+//     Console.Write(output);
+//     return int.Parse(Console.ReadLine());
+// }
+
+
+// ------------62-----------
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[,] nums = new int[size, size];
+
+int num = 1;
+int i = 0;
+int j = 0;
+
+while (num <= size * size)
+{
+    nums[i, j] = num;
+    if (i <= j + 1 && i + j < size - 1)
+        ++j;
+    else if (i < j && i + j >= size - 1)
+        ++i;
+    else if (i >= j && i + j > size - 1)
+        --j;
+    else
+        --i;
+    ++num;
+}
+
+PrintArray(nums);
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
